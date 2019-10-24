9394b39399028a5ecc356ebeb2f9b76105c8651e
nickveliki <53113228+nickveliki@users.noreply.github.com>
2019-10-24 23:43:04 +0200

    Add files via upload

--- component.js
+++ component.js
@@ -2,7 +2,7 @@ const getComponents = require("./getComponent");
 module.exports=({content, type, selfClosing, attributes})=>{
     let ar = getComponents(attributes);
     let cont = getComponents(content);
-    const open = `<${type} ${ar}${selfClosing?"/":""}>`;
-    const close = `</${type}>`;
+    const open = `<${type?type:"div"} ${ar}${selfClosing?"/":""}>`;
+    const close = `</${type?type:"div"}>`;
     return `${open}${selfClosing?"":(cont+close)}`;
 }
